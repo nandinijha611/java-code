41ee967375270fb11b062afac42ac43519946d3e
nandinijha611 <nandini.jha611@gmail.com>
2025-11-09 19:12:13 +0530

    Added code for sum of 2 numbers

---
+++ core-java/Basics/add2numbers.java
@@ -0,0 +1,13 @@
+public class add2numbers {
+    // Add two integers and return the sum
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Example usage
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        System.out.println("Sum: " + add(x, y));
+    }
+}
